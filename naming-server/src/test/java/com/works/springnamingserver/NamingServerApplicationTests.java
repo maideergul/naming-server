c9a2d32e3db278c18f4288dd578d8ce364e073d6
Maide <maideergul@yahoo.com>
2023-11-08 17:11:37 +0300

    master

---
+++ naming-server/src/test/java/com/works/springnamingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.works.springnamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
